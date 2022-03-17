5e29cc0bfb940240206176258a069246edb2dba1
Tommy Huang <13917078913@163.com>
2022-03-17 15:36:58 +0800

    finish policy iteration

--- lab1/PolicyIter.py
+++ lab1/PolicyIter.py
@@ -71,10 +71,49 @@ class PolicyIteration:
         return policy_stable
 
     def draw_value(self):
-        print(self.value)
+        # print(self.value)
+        fig = plt.figure(figsize=(6, 6))
+        plt.subplots_adjust(wspace=0, hspace=0)
+        for x in range(self.n):
+            for y in range(self.m):
+                ax = fig.add_subplot(self.n, self.m, x * self.m + y + 1)
+                ax.axes.get_xaxis().set_ticks([])
+                ax.axes.get_yaxis().set_ticks([])
+                ax.text(0.5, 0.5, round(self.value[x][y], 2), horizontalalignment='center', verticalalignment='center')
+        plt.show()
 
     def draw_policy(self):
         pos = ['U', 'D', 'L', 'R']
+        fig_pos = [(0.5, 1.0), (0.5, 0.0), (0.0, 0.5), (1.0, 0.5)]
+        fig = plt.figure(figsize=(6, 6))
+        plt.subplots_adjust(wspace=0, hspace=0)
+        for x in range(self.n):
+            for y in range(self.m):
+                ax = fig.add_subplot(self.n, self.m, x * self.m + y + 1)
+                ax.axes.get_xaxis().set_ticks([])
+                ax.axes.get_yaxis().set_ticks([])
+
+                ax.spines['bottom'].set_linewidth(2)
+                ax.spines['top'].set_linewidth(2)
+                ax.spines['left'].set_linewidth(2)
+                ax.spines['right'].set_linewidth(2)
+
+                if self.is_terminal(x, y):
+                    ax.set_facecolor('grey')
+                else:
+                    pos = np.array([x, y]) + np.array(self.action[int(self.policy[x][y])])
+                    value = self.value[pos[0], pos[1]]
+
+                    for idx in range(len(self.action)):
+                        next_pos = np.array([x, y]) + np.array(self.action[idx])
+                        if next_pos[0] < 0 or next_pos[0] >= self.n or next_pos[1] < 0 or next_pos[1] >= self.m:
+                            continue
+                        elif abs(self.value[next_pos[0], next_pos[1]] - value) < 0.01:
+                            ax.annotate('', fig_pos[idx], xytext=(0.5, 0.5), xycoords='axes fraction',
+                                        arrowprops={'width': 1.5, 'headwidth': 10, 'color': 'black'})
+
+        plt.show()
+
         for x in range(self.n):
             for y in range(self.m):
                 if self.is_terminal(x, y):
